b7fcbee7c6109848563ebd8262ce447c29939779
Djames16 <djames@brooklynsteamcenter.org>
2024-02-01 10:54:55 -0500

    Day2

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from flask import Flask, render_template, request, redirect
 import pymysql
-from pprint import pprint as print
 from flask_httpauth import HTTPBasicAuth
-from werkzeug.security import generate_password_hash, check_password_hash
-
 
 app=Flask(__name__)
 auth = HTTPBasicAuth()
 @app.route('/')
 def index():
-    user_name=""
     return render_template(
-        "landing.html.jinja",
-        user_name=user_name
+        "landing.html.jinja"
      )
+
+@app.route('/register')
+@auth.login_required
+def index():
+    if request.method=='POST':
+        new_email=request.form["new_user"]
+        new_username=request.form["new_user"]
+        new_possword=request.form["new_user"]
+        cursor=connect.cursor()
+        cursor.execute(f"INSERT INTO `User`(`Username`, `Password`, `Email`) VALUES('{new_user}')")
+        cursor.close()
+        connect.commit()
+def register():
+    return render_template(
+        "Signup.html.jinja"
+     )
+
+connect=pymysql.connect(
+    database="djames_Swipes",
+    user="djames",
+    password="228118717",
+    host="10.100.33.60",
+    cursorclass=pymysql.cursors.DictCursor
+)
