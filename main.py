c42e9efdbf734968dd6c060d95191e99ae67be04
Djames16 <djames@brooklynsteamcenter.org>
2024-02-15 10:54:16 -0500

    Day 6

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, g
 import pymysql
 from flask_httpauth import HTTPBasicAuth
 import flask_login
 
 app=Flask(__name__)
+
+def connect_db():
+    return pymysql.connect(
+        host="10.100.33.60",
+        user="djames",
+        password="228118717",
+        database="djames_Swipes",
+        cursorclass=pymysql.cursors.DictCursor,
+        autocommit=True
+    )
+
+def get_db():
+    '''Opens a new database connection per request.'''        
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db    
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Closes the database connection at the end of request.'''    
+    if hasattr(g, 'db'):
+        g.db.close() 
+
+
 auth = HTTPBasicAuth()
 app.secret_key="something_secret" #change this
 login_manager=flask_login.LoginManager()
@@ -17,13 +41,24 @@ class User:
         self.id=id
     def get_id(self):
         return str(self.id)
+    
+class Posts:
+    is_authenticated=True
+    is_anonymous=False
+    is_active=True
+    def __init__(self,id,Description):
+        self.Description=Description
+        self.id=id
+    def get_id(self):
+        return str(self.id)
+
 @login_manager.user_loader
 def load_user(user_id):
-    cursor=connect.cursor()
+    cursor = get_db().cursor()
     cursor.execute("Select * FROM `User` WHERE `id` =" + str(user_id))
     result=cursor.fetchone()
     cursor.close()
-    connect.commit()
+    get_db().commit()
     if result is None:
         return None
     return User(result["id"], result["Username"])
@@ -32,7 +67,8 @@ def load_user(user_id):
 def landing_page():
     if flask_login.current_user.is_authenticated:
         return redirect('/feed')
-    return render_template(
+    else: 
+        return render_template(
         "landing.html.jinja"
      )
 
@@ -43,10 +79,10 @@ def register():
         new_password=request.form["Password"]
         new_email=request.form["Email"]
         new_description=request.form["Description"]
-        cursor=connect.cursor()
+        cursor = get_db().cursor()
         cursor.execute(f"INSERT INTO `User`(`Username`, `Password`, `Email`, `Description`) VALUES ('{new_username}', '{new_password}','{new_email}', '{new_description}')")
         cursor.close()
-        connect.commit()
+        get_db().commit()
     return render_template(
         "Signup.html.jinja"
      )
@@ -56,29 +92,34 @@ def login():
     if request.method=='POST':
         Username=request.form["Username"]
         Password=request.form["Password"]
-        cursor=connect.cursor()
+        cursor = get_db().cursor()
         cursor.execute(f"Select * FROM `User` WHERE `Username` = '{Username}'")
         result=cursor.fetchone()
         cursor.close()
-        connect.commit()
+        get_db().commit()
         if Password==result["Password"]:
             User=load_user(result['id'])
             flask_login.login_user(User)
             return redirect('/feed')
-        
+
     return render_template(
         "Login.html.jinja"
      )
+
+    
 @app.route('/feed', methods=['GET','POST'])
 @flask_login.login_required
 def feed():
-    return'flask_login.current_user'
+    cursor = get_db().cursor()
+    cursor.execute(f"Select * FROM `Posts`")
+    cursor.close()
+    get_db().commit()
+    return render_template("Feed.html.jinja")
 
-
-connect=pymysql.connect(
-    database="djames_Swipes",
-    user="djames",
-    password="228118717",
-    host="10.100.33.60",
-    cursorclass=pymysql.cursors.DictCursor
-)
+@app.route('/post',methods=['POST'])
+@flask_login.login_required
+def create_post():
+    description=request.form['description']
+    cursor = get_db().cursor()
+    cursor.execute("INSERT INTO `posts(`description`, `user_id`)")
+    user_id=flask_login.current_user.id
